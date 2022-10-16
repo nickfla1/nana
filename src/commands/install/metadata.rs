131ce634db5bca318fb4edcf3a9eb4219c84ff35
nickfla1 <lanterniniflavio@gmail.com>
2022-10-16 21:24:45 +0200

    update depency resolution (#8)

--- src/commands/install/metadata.rs
+++ src/commands/install/metadata.rs
@@ -1,6 +1,7 @@
 use std::{collections::HashMap, sync::Arc};
 
 use async_recursion::async_recursion;
+use indexmap::IndexMap;
 use package_json_schema::PackageJson;
 use semver_rs::{Range, Version};
 use tokio::sync::Mutex;
@@ -13,51 +14,66 @@ use crate::{
 
 use super::fetcher;
 
-type ParsedMetadata = HashMap<String, MetadataVersion>;
+type DependencyTree = HashMap<String, Dependency>;
+
+#[derive(Debug)]
+pub struct Dependency {
+    pub versions: HashMap<String, MetadataVersion>,
+    pub parents: Vec<String>,
+}
 
 pub async fn load_dependencies_metadata<'a>(
     package: &PackageJson,
     progress_handler: Box<dyn ProgressHandler>,
-) -> NanaResult<ParsedMetadata> {
-    let mut metadata = ParsedMetadata::new();
-
+) -> NanaResult<Arc<Mutex<DependencyTree>>> {
+    let dependency_tree = Arc::new(Mutex::new(HashMap::<String, Dependency>::new()));
     let pb = Arc::new(Mutex::new(progress_handler));
 
     if let Some(dependencies) = &package.dependencies {
-        do_load_dependencies_metadata(dependencies, &mut metadata, pb.clone()).await;
+        do_load_dependencies_metadata(
+            "__root__",
+            dependencies,
+            dependency_tree.clone(),
+            pb.clone(),
+        )
+        .await;
     }
 
     pb.lock_owned().await.progress_done();
 
-    Ok(metadata)
+    Ok(dependency_tree.clone())
 }
 
 #[async_recursion]
 async fn do_load_dependencies_metadata(
+    parent: &str,
     dependencies: &Dependencies,
-    metadata: &mut ParsedMetadata,
+    dependency_tree: Arc<Mutex<DependencyTree>>,
     pb: Arc<Mutex<Box<dyn ProgressHandler>>>,
 ) {
     let mut tasks = vec![];
 
     for (name, version) in dependencies {
         let pb = pb.clone();
-        tasks.push(async move { fetch_dependencies_metadata(name, version, pb).await });
+        let dependency_tree = dependency_tree.clone();
+        tasks.push(async move {
+            fetch_dependencies_metadata(parent, name, version, dependency_tree, pb).await
+        });
     }
 
     let joined = futures::future::join_all(tasks).await;
-
     for res in joined {
         match res {
-            Ok(mut list) => {
-                let meta_version = find_best_matching_version(&mut list);
-                let key = meta_version.get_key();
-                metadata.insert(key, meta_version.clone());
-
-                if let Some(deps) = &meta_version.dependencies {
-                    do_load_dependencies_metadata(deps, metadata, pb.clone()).await;
-                }
+            Ok((Some(name), Some(dependencies))) => {
+                do_load_dependencies_metadata(
+                    &name,
+                    &dependencies,
+                    dependency_tree.clone(),
+                    pb.clone(),
+                )
+                .await;
             }
+            Ok((_, _)) => {}
             Err(error) => {
                 panic!("Error: {}", error);
             }
@@ -66,14 +82,43 @@ async fn do_load_dependencies_metadata(
 }
 
 async fn fetch_dependencies_metadata(
+    parent: &str,
     name: &String,
     version: &str,
+    dependency_tree: Arc<Mutex<DependencyTree>>,
     pb: Arc<Mutex<Box<dyn ProgressHandler>>>,
-) -> NanaResult<Vec<MetadataVersion>> {
-    let metadata = fetcher::fetch_metadata(name, Some(pb.clone())).await?;
-    let version_list = parse_metadata(metadata, version)?;
+) -> NanaResult<(Option<String>, Option<IndexMap<String, String>>)> {
+    if let Some(dep) = dependency_tree.lock().await.get_mut(name) {
+        let range = Range::new(version).parse()?;
 
-    Ok(version_list)
+        for version in dep.versions.keys() {
+            if range.test(&Version::new(version).parse()?) {
+                dep.parents.push(parent.into());
+
+                return Ok((None, None));
+            }
+        }
+    }
+
+    let metadata = fetcher::fetch_metadata(name, Some(pb.clone())).await?;
+    let mut version_list = parse_metadata(metadata, version)?;
+    let meta_version = find_best_matching_version(&mut version_list);
+
+    let mut tree_lock = dependency_tree.lock().await;
+    if let Some(dep) = tree_lock.get_mut(name) {
+        dep.versions
+            .insert(meta_version.version.clone(), meta_version.clone());
+    } else {
+        tree_lock.insert(
+            name.clone(),
+            Dependency {
+                versions: HashMap::from([(meta_version.version.clone(), meta_version.clone())]),
+                parents: vec![],
+            },
+        );
+    }
+
+    Ok((Some(name.clone()), meta_version.dependencies.clone()))
 }
 
 fn parse_metadata(metadata: Metadata, raw_range: &str) -> NanaResult<Vec<MetadataVersion>> {
